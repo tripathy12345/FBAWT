2eb3ba63940127589a46e2ed08bde8d5a6640f5a
Rajesh Tripathy <rajesh.nitr11@gmail.com>
2023-04-22 17:51:37 +0530

    Add files via upload

---
+++ FBSE_domain_wavelet_function.m
@@ -0,0 +1,25 @@
+function ymw=FBSE_domain_wavelet_function(freq1, mb, ma, gamma, N)
+w=freq1';
+aw=abs(w);
+ymw=zeros(N,1);
+wn=w(mb);
+wm=w(ma);
+an=1/(2*gamma*wn);
+am=1/(2*gamma*wm);
+pbn=(1+gamma)*wn;
+mbn=(1-gamma)*wn;
+pbm=(1+gamma)*wm;
+mbm=(1-gamma)*wm;
+
+for k=1:N
+   if ((aw(k)>=pbn) && (aw(k)<=mbm))
+       ymw(k)=1;
+   elseif ((aw(k)>=mbm) && (aw(k)<=pbm))
+       %ymw(k)=complex(cos(w(k)/2),-sin(w(k)/2))*cos(pi*EWT_beta(am*(aw(k)-mbm))/2);   
+       ymw(k)=cos(pi*EWT_beta(am*(aw(k)-mbm))/2);   
+   elseif ((aw(k)>=mbn) && (aw(k)<=pbn))
+       %ymw(k)=complex(cos(w(k)/2),-sin(w(k)/2))*sin(pi*EWT_beta(an*(aw(k)-mbn))/2);
+       ymw(k)=sin(pi*EWT_beta(an*(aw(k)-mbn))/2);
+   end
+end
+end
